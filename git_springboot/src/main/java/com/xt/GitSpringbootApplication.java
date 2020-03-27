2a225cb42a439945ea28033282ad8aa5e5d8d37d
光荣 <gr_xbiubiu@163.com>
2020-03-27 21:59:10 +0800

    保存工作现场

--- git_springboot/src/main/java/com/xt/GitSpringbootApplication.java
+++ git_springboot/src/main/java/com/xt/GitSpringbootApplication.java
@@ -13,7 +13,14 @@ public class GitSpringbootApplication {
         System.out.println("修改二次");
         System.out.println("在dev里修改");
 
+
         System.out.println("123");
+
+        System.out.println("在dev里第一次改  123");
+        System.out.println("master 345");
+
+        System.out.println("测试保存工作现场stash 1.0");
+
     }
 
 }
