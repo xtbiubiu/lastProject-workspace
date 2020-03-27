e522540541b5068bdad096961abc262520fb6f37
光荣 <gr_xbiubiu@163.com>
2020-03-27 21:09:33 +0800

    init 初始化仓库

---
+++ git_springboot/src/test/java/com/xt/GitSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
